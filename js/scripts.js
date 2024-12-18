1e4ccc0f7c5862c67f542c282c1890c4cf0bc364
Dan Bray <d.bray33@gmail.com>
2024-12-17 22:43:18 -0500

    SERVICE ITEMS fixed hover/tap on mobile. Removed front side text from back side

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
   const serviceItems = document.querySelectorAll('.service-item');
 
   serviceItems.forEach((item) => {
-    item.addEventListener('click', function (event) {
-      event.stopPropagation(); // Prevent the event from bubbling up to the document
-      const inner = this.querySelector('.service-item-inner');
-      const isRotated = inner.classList.contains('rotate');
-
-      // Rotate back if clicking on the backside
-      if (isRotated && event.target.closest('.service-item-back')) {
-        inner.classList.remove('rotate');
-      } else {
-        // Rotate the clicked card and rotate back all other cards
-        serviceItems.forEach((otherItem) => {
-          if (otherItem !== item) {
-            otherItem
-              .querySelector('.service-item-inner')
-              .classList.remove('rotate');
-          }
-        });
-        inner.classList.toggle('rotate');
-      }
+    item.addEventListener('touchstart', function () {
+      serviceItems.forEach((el) => el.classList.remove('service-item-hover'));
+      this.classList.add('service-item-hover');
     });
   });
 
-  document.addEventListener('click', function () {
-    serviceItems.forEach((item) => {
-      item.querySelector('.service-item-inner').classList.remove('rotate');
-    });
+  document.addEventListener('touchstart', function (event) {
+    if (!event.target.closest('.service-item')) {
+      serviceItems.forEach((el) => el.classList.remove('service-item-hover'));
+    }
   });
 });
