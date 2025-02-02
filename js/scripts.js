e096b21a9d7611062f609cd0b699125d3b79dfee
Dan Bray <d.bray33@gmail.com>
2025-02-01 22:28:11 -0500

    back to top added

--- js/scripts.js
+++ js/scripts.js
@@ -583,3 +583,20 @@ document.addEventListener('DOMContentLoaded', () => {
 // //////////////////////////////////////////////
 // //////////////////////////////////////////////
 // //////////////////////////////////////////////
+// Show button when scrolling down
+window.onscroll = function () {
+  let button = document.getElementById('backToTop');
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    button.classList.add('show');
+  } else {
+    button.classList.remove('show');
+  }
+};
+
+// Smooth scroll to top
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
