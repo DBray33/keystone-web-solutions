6ddb5a584cc72c279512429082750b600fe5f984
Dan Bray <d.bray33@gmail.com>
2025-02-27 14:10:19 -0500

    removed clints folder and stripe-pay folders for now

--- stripe-pay/secrets.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Keep your Stripe API key protected by including it as an environment variable
-// or in a private script that does not publicly expose the source code.
-
-// This is your test secret API key.
-$stripeSecretKey = 'sk_test_51QqKcUJXGPtgK9Fks52TLzC3E9jjVgPr7CI2NQjPoqeHj9Ker8DsvD1MMH8IAfOieNnpx9eh0PhKxmKu8bHxbkNq00vSbgRq2M';
